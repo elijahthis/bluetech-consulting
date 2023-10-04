4cb5755d209a519c01e17b39f5930e58fe5ecb20
elijahthis <emperorelijah007@gmail.com>
2023-10-04 18:38:43 +0100

    feat: cloudinary

--- pageParts/home/Hero.tsx
+++ pageParts/home/Hero.tsx
@@ -2,7 +2,12 @@ export const Hero = () => {
 	return (
 		<section>
 			<video className="w-full" muted autoPlay loop>
-				<source src={"/videos/short-vid.mp4"} type="video/mp4" />
+				<source
+					src={
+						"https://res.cloudinary.com/dywp91yx7/video/upload/v1696440993/bluetech-consulting/short-vid.mp4"
+					}
+					type="video/mp4"
+				/>
 				Your browser does not support the video tag.
 			</video>
 			<div className="flex flex-row items-center gap-4 justify-between py-[41px] px-[51px] pl-[130px] bg-[#051C2C] ">
