83b1a936d6ea2627789df5ecc7b5a5603025f840
elijahthis <emperorelijah007@gmail.com>
2023-10-10 11:03:20 +0100

    feat: View Course

--- app/courses/[id]/page.tsx
+++ app/courses/[id]/page.tsx
@@ -1,3 +1,6 @@
+import CourseHighlightsCard from "@/components/CourseHighlightsCard";
+import CourseInfoCard from "@/components/CourseInfoCard";
+import OverviewCard from "@/components/OverviewCard";
 import TitleBanner from "@/components/TitleBanner";
 import { courseList } from "@/data/constants";
 
@@ -25,6 +28,16 @@ const CoursePage = ({ params }: { params: { id: string } }) => {
 					{ label: currentCourse.short_title, link: currentCourse.page_url },
 				]}
 			/>
+			<div
+				className="pt-[77px] pb-10 px-4 lg:px-[102px] bg-[#F0F9FF] flex flex-col items-stretch gap-6 lg:grid  "
+				style={{ gridTemplateColumns: "61fr 39fr" }}
+			>
+				<OverviewCard courseData={currentCourse} />
+				<div className="flex flex-col items-stretch gap-6">
+					<CourseInfoCard courseData={currentCourse} />
+					<CourseHighlightsCard courseData={currentCourse} />
+				</div>
+			</div>
 		</main>
 	);
 };
--- components/SmallNav/index.tsx
+++ components/SmallNav/index.tsx
@@ -9,7 +9,7 @@ const SmallNav = ({
 	return (
 		<div className="grid place-items-center absolute w-full bottom-[-27px] ">
 			<div
-				className="rounded bg-white w-max px-6 py-4 flex flex-row items-center gap-4 text-[18px] leading-[24px] font-bold text-[#696969] "
+				className="rounded bg-white w-max px-4 lg:px-6 py-3 lg:py-4 flex flex-row items-center gap-[10px] lg:gap-4 text:sm lg:text-[18px] leading-[24px] font-bold text-[#696969] "
 				style={{ boxShadow: "0px 4px 10px 0px rgba(0, 0, 0, 0.19)" }}
 			>
 				<Link href="/" className="text-[#696969]">
