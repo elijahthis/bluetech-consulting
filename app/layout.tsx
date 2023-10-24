893c471e41fdb1301a35c21647f38d871d2a9023
elijahthis <emperorelijah007@gmail.com>
2023-10-24 15:32:41 +0100

    fix: added CSP + middlewares

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import Footer from "@/components/Footer";
+import { headers } from "next/headers";
+import Script from "next/script";
 
 const roboto = Roboto({
 	weight: ["100", "300", "400", "500", "700", "900"],
@@ -19,12 +21,19 @@ export default function RootLayout({
 }: {
 	children: React.ReactNode;
 }) {
+	const nonce = headers().get("x-nonce")!;
+
 	return (
 		<html lang="en">
 			<body>
 				<Header />
 				<div className="mt-[72px] lg:mt-[100px] ">{children}</div>
 				<Footer />
+				<Script
+					src="https://www.googletagmanager.com/gtag/js"
+					strategy="afterInteractive"
+					nonce={nonce}
+				/>
 			</body>
 		</html>
 	);
